1919f2225c6de6ffd37c3c33125406153cec1369
benjamin-t-brown <benjamin.t.brown@hotmail.com>
2025-05-14 10:02:40 -0600

    update makefile

--- src/miyoo-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export LD_LIBRARY_PATH="/mnt/SDCARD/ROMS/examples:/customer/lib:/lib:/lib:/config/lib:/mnt/SDCARD/miyoo/lib:/mnt/SDCARD/.tmp_update/lib:/mnt/SDCARD/.tmp_update/lib/parasyte"
-
-PID=$(pidof MainUI)
-echo $PID
-kill -STOP $PID
-./SDL2W_EXAMPLE
-kill -CONT $PID
